98af85ec99709e7704d7960e9e94f4fa1f2401c1
limgs97dev <limgs97dev@gmail.com>
2024-10-06 18:11:11 +0900

    init

---
+++ src/main/kotlin/com/escape/pixao/PixaoApiApplication.kt
@@ -0,0 +1,11 @@
+package com.escape.pixao
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PixaoApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<PixaoApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/escape/pixao/PixaoApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.escape.pixao
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PixaoApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
